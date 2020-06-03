740c4f9147cc9c753b72d989b91ca6a802898dfe
Emanuel VF <emanuelvillafernandez@gmail.com>
2020-06-03 17:11:23 -0300

    SetDateViewController coded

--- ThisWeek/SetDateViewController.swift
+++ ThisWeek/SetDateViewController.swift
@@ -8,32 +8,108 @@
 
 import UIKit
 
+protocol SetDateViewControllerDelegate : class {
+    func setAFutureDay(_ sender : SetDateViewController)
+    func deleteAFutureDay(_ sender : SetDateViewController)
+}
+
 class SetDateViewController: UIViewController {
+    
+    var dayToRemember : Date?
+    var newDayToRemember : Date?
+    var editingMode : Bool?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+//    MARK: - ViewController LifeCycle
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if let fittedSize = mainStack?.sizeThatFits(UIView.layoutFittingCompressedSize){
+            preferredContentSize = CGSize(width: fittedSize.width * ThisWeekViewController.Defaults.pickerSizeFactor, height: fittedSize.height * ThisWeekViewController.Defaults.pickerSizeFactor)
+        }
     }
 
-    @IBOutlet weak var mainStack: UIStackView!
+    //  MARK:-  Delegation
+    weak var delegate : SetDateViewControllerDelegate?
     
-    @IBOutlet weak var titleLabel: UILabel!
-    
-    @IBOutlet weak var pickerDate: UIDatePicker!
-    
-    @IBOutlet weak var rightButton: UIButton!
-    @IBOutlet weak var leftButton: UIButton!
-    
-    @IBOutlet weak var lastButton: UIButton!
+    //    MARK:- Actions
     
     
     @IBAction func leftButtonPressed(_ sender: UIButton) {
+        presentingViewController?.dismiss(animated: true)
     }
     
     @IBAction func rightButtonPressed(_ sender: UIButton) {
+        if editingMode! == false{
+            rightButton.setTitle("Elegir", for: .normal)
+            pickerDate.isUserInteractionEnabled = true
+            editingMode = true
+            titleLabel.text = "Elegi el dia puto"
+        }else{
+            newDayToRemember = pickerDate.date
+            self.delegate?.setAFutureDay(self)
+            presentingViewController?.dismiss(animated: true)
+        }
     }
     
     @IBAction func lastButtonPressed(_ sender: UIButton) {
+        self.delegate?.deleteAFutureDay(self)
+        presentingViewController?.dismiss(animated: true)
     }
+    
+    
+    //    MARK: - Outlets
+    @IBOutlet weak var mainStack: UIStackView!
+    
+    @IBOutlet weak var titleLabel: UILabel!{
+        didSet{
+            if dayToRemember != nil{
+                titleLabel.text = "Queres saber tu dia guachin?"
+            }else{
+                titleLabel.text = "Elegi el dia puto"
+            }
+        }
+    }
+    
+    @IBOutlet weak var pickerDate: UIDatePicker!{
+        didSet{
+            if dayToRemember != nil{
+                pickerDate.date = Date().addingTimeInterval(TimeInterval(8*ThisWeek.Defaults.oneDay))
+                pickerDate.isUserInteractionEnabled = true
+                editingMode = true
+            }else{
+                pickerDate.date = dayToRemember!
+                pickerDate.isUserInteractionEnabled = false
+                editingMode = false
+            }
+        }
+    }
+    
+    @IBOutlet weak var rightButton: UIButton!{
+        didSet{
+            if dayToRemember != nil{
+                rightButton.setTitle("Editar", for: .normal)
+            }else{
+                rightButton.setTitle("Elegir", for: .normal)
+            }
+        }
+    }
+    
+    
+    @IBOutlet weak var leftButton: UIButton!{
+        didSet{
+            leftButton.setTitle("Cancelar", for: .normal)
+        }
+    }
+    
+    @IBOutlet weak var lastButton: UIButton!{
+        didSet{
+            if dayToRemember != nil{
+                lastButton.setTitle("Eliminar Recordatorio", for: .normal)
+                lastButton.isHidden = false
+            }else{
+                lastButton.isHidden = true
+            }
+        }
+    }
+    
+    
 }
