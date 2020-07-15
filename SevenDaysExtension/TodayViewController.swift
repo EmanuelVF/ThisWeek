9ed810f30e1a51ea934afa434815c3ae4e68ea5e
Emanuel VF <emanuelvillafernandez@gmail.com>
2020-07-15 18:28:39 -0300

    Adding the Today Extension

---
+++ SevenDaysExtension/TodayViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TodayViewController.swift
+//  SevenDaysExtension
+//
+//  Created by Emanuel on 15/07/2020.
+//  Copyright © 2020 Emanuel. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.extensionContext?.widgetLargestAvailableDisplayMode = .expanded 
+        // Do any additional setup after loading the view.
+    }
+        
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+    func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
+        if activeDisplayMode == .compact {
+            self.preferredContentSize = maxSize
+        } else if activeDisplayMode == .expanded {
+            self.preferredContentSize = CGSize(width: maxSize.width, height: 150)
+        }
+    }
+    
+}
